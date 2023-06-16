23455cea0fd31a9cf356a5c278210ddf3e235e85
Daniel Hines <d4hines@gmail.com>
2023-06-16 10:42:29 -0400

    use zero as unit

--- src/lambda_calculus/church_numerals.js
+++ src/lambda_calculus/church_numerals.js
@@ -2,8 +2,9 @@ const zero = (f) => (x) => x;
 const one = (f) => (x) => f(x);
 const plus = (m) => (n) => (f) => (x) => m(f)(n(f)(x));
 const succ = (n) => (f) => (x) => plus(one)(n)(f)(x);
-const true_ = (a) => (_) => a();
-const false_ = (_) => (b) => b();
+const unit = zero;
+const true_ = (a) => (_) => a(unit);
+const false_ = (_) => (b) => b(unit);
 const if_ = (pred) => (a) => (b) => pred(a)(b);
 const is_zero = (n) => n((_) => false_)(true_);
 
@@ -20,8 +21,8 @@ const pred = (() => {
 
 const z = g => (x => g(v => x(x)(v)))(x => g(v => x(x)(v)))
 
-const plus_z = z(recurse => a => b => if_(is_zero(b))(() => a)(() => recurse(succ(a))(pred(b))));
-const minus_z = z(recurse => a => b => if_(is_zero(b))(() => a)(() => recurse(pred(a))(pred(b))));
+const plus_z = z(recurse => a => b => if_(is_zero(b))((_) => a)((_) => recurse(succ(a))(pred(b))));
+const minus_z = z(recurse => a => b => if_(is_zero(b))((_) => a)((_) => recurse(pred(a))(pred(b))));
 
 const church_to_int = (cn) => cn((x) => x + 1)(0);
 const print_church = (label, x) => console.log(label + ": " + church_to_int(x));
@@ -33,15 +34,15 @@ print_church("plus 1 (succ zero)", plus(one)(succ(zero)));
 print_church("pred(plus 1 (succ zero))", pred(plus(one)(succ(zero))));
 print_church(
   "if zero test: true",
-  if_(is_zero(zero))(() => one)(() => zero)
+  if_(is_zero(zero))((_) => one)((_) => zero)
 );
 print_church(
   "if zero test: false",
-  if_(is_zero(one))(() => one)(() => zero)
+  if_(is_zero(one))((_) => one)((_) => zero)
 );
 print_church(
   "if zero test: false",
-  if_(is_zero(succ(zero)))(() => one)(() => zero)
+  if_(is_zero(succ(zero)))((_) => one)((_) => zero)
 );
 print_church("plus_z: 1 + 1", plus_z(one)(one));
 print_church("minus_z: 2 - 1", minus_z(succ(one))(one));
